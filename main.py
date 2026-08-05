cc36f0f4c97ecd7a01dcb16b3432ba9f380df947
vitoriojoaopedro67 <0074868@sesi-es.org.br>
2026-08-05 15:25:10 -0300

    inicia projeto de gerenciamento de biblioteca

---
+++ main.py
@@ -0,0 +1,9 @@
+import csv 
+import os
+
+ARQUIVO_CSV = "livros.csv"
+# Arquivo utilizado para armazenar os livros cadastrados
+
+CAMPOS = ["titulo", "autor", "ano", "isbn", "status"]
+# Função para cadastrar um novo livro
+
