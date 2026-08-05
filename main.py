6d4320a22509a4b2f3cbe01d776b7d676e521f93
vitoriojoaopedro67 <0074868@sesi-es.org.br>
2026-08-05 15:32:01 -0300

    implementa funcao de carregamento de livros a partir do CSV

--- main.py
+++ main.py
@@ -7,3 +7,15 @@ ARQUIVO_CSV = "livros.csv"
 CAMPOS = ["titulo", "autor", "ano", "isbn", "status"]
 # Função para cadastrar um novo livro
 
+def carregar_livros():
+# Lê o CSV com os livros. Se o arquivo ainda não existe, começamos com uma lista vazia mesmo.
+
+    if not os.path.exists(ARQUIVO_CSV):
+        return lista_livros
+
+    with open(ARQUIVO_CSV, mode="r", newline="", encoding="utf-8") as arquivo:
+        leitor = csv.DictReader(arquivo)
+        for linha in leitor:
+            lista_livros.append(linha)
+
+    return lista_livros
